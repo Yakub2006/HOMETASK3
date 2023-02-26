ae11864df2804d4958cba8e7965d06674428bf3f
Yakub2006 <114349408+Yakub2006@users.noreply.github.com>
2023-02-26 13:35:13 +0300

    Update Task2.java

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Task2 {
+public class Task2 {
+   public  static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[10];
+        boolean number = false;
+        int x = in.nextInt();
+        int i;
+        for ( i = 0; i < 10; i++) {
+            arr[i] = (int) (Math.random() * 100);
+            if(x == arr[i]){
+                number = true;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        if (number) {
+            arr[i] = 0;
+            System.out.println(Arrays.toString(arr));
+        } else {
+            System.out.println("No");
+
+        }
+    }
 }
