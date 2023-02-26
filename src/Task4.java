d925069626b700439fb51d5062a14e62fc876e00
Yakub2006 <114349408+Yakub2006@users.noreply.github.com>
2023-02-26 13:37:18 +0300

    Update Task4.java

--- src/Task4.java
+++ src/Task4.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Task4 {
+import java.util.Arrays;
+
+
+public class Task4 {
+  public static void main(String[] args){
+        int[] arr1 = new int[5];
+        int[] arr2 = new int[5];
+        for (int i = 0; i < 5; i++){
+            arr1[i] = (int) (Math.random() * 10);
+            arr2[i] = (int) (Math.random() * 10);
+        }
+        int midNum1 = Arrays.stream(arr1).sum() / 5;
+        int midNum2 = Arrays.stream(arr2).sum() / 5;
+        if (midNum1 > midNum2){
+            System.out.println("БОЛЬШЕ");
+        }
+        else if(midNum1 < midNum2){
+            System.out.println("МЕНЬШЕ");
+        }
+        else{
+            System.out.println("РАВНЫ");
+        }
+    }
 }
